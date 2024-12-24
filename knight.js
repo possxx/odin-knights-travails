d27b2277b5d1632d91464dede22babd418fc399d
possxx <168581964+possxx@users.noreply.github.com>
2024-12-24 15:17:57 +0100

    Finish logic for traversing board with shortest path

--- knight.js
+++ knight.js
@@ -22,3 +22,63 @@ function possibleMoves(position) {
     return x >= 0 && x <= 7 && y >= 0 && y <= 7;
   });
 }
+
+function inbetweenMove(start, end, depth) {
+  let queue = [];
+
+  while (depth > 1) {
+    if (!queue.length) {
+      queue = possibleMoves(start);
+      depth--;
+      continue;
+    }
+
+    const newQueue = [];
+    queue.forEach((item) => {
+      possibleMoves(item).forEach((move) => newQueue.push(move));
+    });
+
+    queue = newQueue;
+    depth--;
+  }
+
+  const newMoves = possibleMoves(end);
+
+  return newMoves.filter((move) => {
+    return JSON.stringify(queue).includes(move);
+  });
+}
+
+function knightMoves(start, end) {
+  let queue = possibleMoves(start);
+  let depth = 0;
+  let endFound = false;
+
+  while (!endFound) {
+    const newQueue = [];
+    queue.forEach((item) => {
+      if (item[0] === end[0] && item[1] === end[1]) endFound = true;
+      possibleMoves(item).forEach((move) => newQueue.push(move));
+    });
+
+    queue = newQueue;
+    depth += 1;
+  }
+
+  let allMoves = [start, end];
+  let depthStore = depth;
+
+  while (depth > 1) {
+    const inbetweenMoves = inbetweenMove(start, end, depth);
+    allMoves.splice(1, 0, inbetweenMoves[0]);
+    depth--;
+    end = inbetweenMoves[0];
+  }
+
+  console.log(`=> You made it in ${depthStore} moves! Here's your path:`);
+  allMoves.forEach((move) => {
+    console.log(move);
+  });
+}
+
+knightMoves([0, 0], [3, 3]);

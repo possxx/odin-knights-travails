9283613387b8e13f41e55bc0a4a55c3537b38dfe
possxx <168581964+possxx@users.noreply.github.com>
2025-01-03 19:52:39 +0100

    Improved logic by making it more efficient

--- knight.js
+++ knight.js
@@ -22,3 +22,31 @@ function possibleMoves(position) {
     return x >= 0 && x <= 7 && y >= 0 && y <= 7;
   });
 }
+
+function knightMoves(start, end) {
+  const queue = [[start]];
+  const visitedSqu = new Set();
+
+  while (queue.length) {
+    const path = queue.shift();
+    const currDepth = path.length - 1;
+
+    if (
+      path[path.length - 1][0] === end[0] &&
+      path[path.length - 1][1] === end[1]
+    ) {
+      console.log(`You made it in ${currDepth} moves! Here's your path:`);
+      console.log(path);
+      break;
+    }
+
+    for (const move of possibleMoves(path[path.length - 1])) {
+      if (!visitedSqu.has(move)) {
+        queue.push([...path, move]);
+        visitedSqu.add(move);
+      }
+    }
+  }
+}
+
+knightMoves([0, 0], [7, 7]);
